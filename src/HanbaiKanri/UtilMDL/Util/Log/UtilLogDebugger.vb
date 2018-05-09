1d96aafe766e1fd71047bf8bb2fc3a16eb7bb801
田頭勇也 <denden@mono-graph.com>
2018-05-09 22:58:37 +0900

    欠損ファイルの追加

---
+++ src/HanbaiKanri/UtilMDL/Util/Log/UtilLogDebugger.vb
@@ -0,0 +1,100 @@
+Imports System.IO
+Imports System.Text
+
+
+Namespace Log
+    '===============================================================================
+    '
+    '  ユーティリティクラス
+    '    （クラス名）    UtilLogDebugger
+    '    （処理機能名）      ログ出力拡張機能を提供する
+    '    （本MDL使用前提）   UtilLogWriterがプロジェクトに取り込まれていること
+    '    （備考）            UtilLogWriterを継承
+    '
+    '===============================================================================
+    '  履歴  名前          日  付      マーク      内容
+    '-------------------------------------------------------------------------------
+    '  (1)   Jun.Takagi    2006/04/18             新規
+    '-------------------------------------------------------------------------------
+    Public Class UtilLogDebugger
+        Inherits UtilLogWriter
+
+        '===============================================================================
+        'メンバー定数定義
+        '===============================================================================
+        Public Const LOG_DEBUG As Short = 1   'ログ出力タイプ＝デバッグ
+        Public Const LOG_INFO As Short = 2    'ログ出力タイプ＝インフォメーション
+        Public Const LOG_ERR As Short = 3     'ログ出力タイプ＝エラー
+
+        '===============================================================================
+        'メンバー変数定義
+        '===============================================================================
+        Private _debugFlg As Boolean                'デバッグモード
+
+        '===============================================================================
+        'プロパティ(アクセサ)
+        '===============================================================================
+        Public Property debugFlg() As Boolean
+            'Geter--------
+            Get
+                debugFlg = _debugFlg
+            End Get
+            'Setter-------
+            Set(ByVal Value As Boolean)
+                _debugFlg = Value
+                Call MyBase.writeLine("デバッグモードを[" & _debugFlg.ToString & "]に変更します。")
+            End Set
+        End Property
+
+        '===============================================================================
+        ' コンストラクタ
+        '   ●入力パラメタ   ：  prmFileNm           Logファイル名(フルパス)
+        '                       prmDebugFlg         デバッグモード
+        '                       <prmConsoleWrite>   コンソール出力するかどうか
+        '===============================================================================
+        ''' <summary>
+        ''' コンストラクタ
+        ''' </summary>
+        ''' <param name="prmFileNm">Logファイル名(フルパス)</param>
+        ''' <param name="prmDebugFlg">デバッグモード</param>
+        ''' <param name="prmConsoleWrite">コンソール出力するかどうか</param>
+        ''' <remarks></remarks>
+        Public Sub New(ByVal prmFileNm As String, _
+                       ByVal prmDebugFlg As Boolean, _
+                       Optional ByVal prmConsoleWrite As Boolean = True)
+            MyBase.New(prmFileNm, prmConsoleWrite)
+            _debugFlg = prmDebugFlg
+        End Sub
+
+        '-------------------------------------------------------------------------------
+        '   拡張ログ出力
+        '   （処理概要）ログ出力タイプがデバッグのものはデバッグモード=trneの場合のみ出力する
+        '   ●入力パラメタ：iLogType ログ出力タイプ(LOG_DEBUG/LOG_INFO/LOG_ERR)
+        '                 ：他 mybase.writeline参照
+        '   ●メソッド戻り値 ：なし
+        '   ●発生例外       ：Exception
+        '                                               2006.04.18 Created By Jun.Takagi
+        '-------------------------------------------------------------------------------
+        ''' <summary>
+        ''' 拡張ログ出力 ログ出力タイプがデバッグのものはデバッグモード=trneの場合のみ出力する
+        ''' </summary>
+        ''' <param name="prmLogType">ログ出力タイプ(LOG_DEBUG/LOG_INFO/LOG_ERR)</param>
+        ''' <param name="prmOutPut">YYYY/MM/DD HH:MM:DD   エラーコード＆エラーメッセージ</param>
+        ''' <param name="prmOutPut2">SQL文など追加メッセージ(指定時のみ出力)</param>
+        ''' <remarks></remarks>
+        Public Shadows Sub writeLine(ByVal prmLogType As Short, _
+                                     ByVal prmOutPut As String, _
+                                     Optional ByVal prmOutPut2 As String = "")
+
+            If prmLogType = LOG_DEBUG And _debugFlg Then
+                Call MyBase.writeLine("DEBUG   " & Space(1) & prmOutPut, prmOutPut2)
+            ElseIf prmLogType = LOG_INFO Then
+                Call MyBase.writeLine("INFO    " & Space(1) & prmOutPut, prmOutPut2)
+            ElseIf prmLogType = LOG_ERR Then
+                Call MyBase.writeLine("ERR*****" & Space(1) & prmOutPut, prmOutPut2)
+            End If
+
+
+        End Sub
+    End Class
+End Namespace
---
+++ src/HanbaiKanri/UtilMDL/Util/Log/UtilLogWriter.vb
@@ -0,0 +1,135 @@
+Imports System.IO
+Imports System.Text
+
+
+Namespace Log
+    '===============================================================================
+    '
+    '  ユーティリティクラス
+    '    （クラス名）    UtilLogWrier
+    '    （処理機能名）      ログ出力機能を提供する
+    '    （本MDL使用前提）   特になし
+    '    （備考）            consoleWrite()をFalseに設定するとコンソール出力しなくなる
+    '
+    '===============================================================================
+    '  履歴  名前          日  付      マーク      内容
+    '-------------------------------------------------------------------------------
+    '  (1)   Jun.Takagi    2006/04/17             新規
+    '-------------------------------------------------------------------------------
+    Public Class UtilLogWriter
+
+        '===============================================================================
+        'メンバー変数定義
+        '===============================================================================
+        Private _fileNm As String           'ログファイル名
+        Private _consoleWrite As Boolean    'コンソール出力するかどうか
+
+        '===============================================================================
+        'プロパティ(アクセサ)
+        '===============================================================================
+        Public ReadOnly Property fileNm() As String
+            'Geter--------
+            Get
+                fileNm = _fileNm
+            End Get
+            'Setter-------
+            'なし
+        End Property
+        Public Property consoleWrite() As Boolean
+            'Geter--------
+            Get
+                consoleWrite = _consoleWrite
+            End Get
+            'Setter-------
+            Set(ByVal Value As Boolean)
+                Dim wkVal As String
+                If Value Then
+                    wkVal = "コンソール出力を開始します。"
+                Else
+                    wkVal = "コンソール出力を停止します。"
+                End If
+                _consoleWrite = True
+                writeLine(wkVal)
+                _consoleWrite = Value
+            End Set
+        End Property
+
+        '===============================================================================
+        ' コンストラクタ
+        '   ●入力パラメタ   ：  prmFileNm           Logファイル名(フルパス)
+        '                       <prmConsoleWrite>   コンソール出力するかどうか
+        '===============================================================================
+        ''' <summary>
+        ''' コンストラクタ
+        ''' </summary>
+        ''' <param name="prmFileNm">Logファイル名(フルパス)</param>
+        ''' <param name="prmConsoleWrite">コンソール出力するかどうか</param>
+        ''' <remarks></remarks>
+        Public Sub New(ByVal prmFileNm As String, Optional ByVal prmConsoleWrite As Boolean = False)
+            _fileNm = prmFileNm
+            _consoleWrite = prmConsoleWrite
+        End Sub
+
+        '-------------------------------------------------------------------------------
+        '   ログ出力
+        '   （処理概要）指定された文字列をログ出力する
+        '               書式：１行目     YYYY/MM/DD HH:MM:DD   エラーコード＆エラーメッセージ
+        '   　　　            ２行目     → SQL文など追加メッセージ(指定時のみ出力)
+        '   ●入力パラメタ  ：prmOutPut      出力ログ
+        '                   ：<prmOutPut2>   出力ログ２(SQL文などを想定)　改行後に出力
+        '   ●メソッド戻り値 ：なし
+        '   ●発生例外       ：Exception
+        '                                               2006.04.17 Created By Jun.Takagi
+        '-------------------------------------------------------------------------------
+        ''' <summary>
+        ''' ログ出力 指定された文字列をログ出力する
+        ''' </summary>
+        ''' <param name="prmOutPut">YYYY/MM/DD HH:MM:DD   エラーコード＆エラーメッセージ</param>
+        ''' <param name="prmOutPut2">SQL文など追加メッセージ(指定時のみ出力)</param>
+        ''' <remarks></remarks>
+        Public Sub writeLine(ByVal prmOutPut As String, _
+                             Optional ByVal prmOutPut2 As String = "")
+            Dim log As StreamWriter
+            Dim outStr As String
+            Try
+                'ログファイルオープン
+                log = New StreamWriter(_fileNm, True, Encoding.Default)
+
+                Try
+                    '文字列編集
+                    outStr = Now.ToString("G") & Space(3) & prmOutPut
+
+                    '出力
+                    log.WriteLine(outStr)
+                    Debug.WriteLine(outStr)
+                    If _consoleWrite Then
+                        Console.WriteLine(outStr) 'コンソール出力
+                    End If
+
+                    'オプションパラメタが設定されている場合はそちらも出力
+                    If (Not prmOutPut2.Equals("")) Then
+                        outStr = prmOutPut2
+                        log.WriteLine(outStr)
+                        Debug.WriteLine(outStr)
+                        If _consoleWrite Then
+                            Console.WriteLine(outStr) 'コンソール出力
+                        End If
+                    End If
+
+                Catch ex As Exception
+                    Throw ex
+                Finally
+                    'ファイルクローズ
+                    log.Close()
+                End Try
+
+            Catch ex2 As Exception
+                Debug.WriteLine(ex2.Message)
+                Debug.WriteLine(ex2.StackTrace)
+                Throw ex2
+            End Try
+
+        End Sub
+    End Class
+
+End Namespace
