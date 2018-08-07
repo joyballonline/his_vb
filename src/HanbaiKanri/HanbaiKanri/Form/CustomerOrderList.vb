d6a9776157cbd1bfc6037aeec87c0a508d23ceaf
田頭勇也 <denden@mono-graph.com>
2018-08-07 21:30:07 +0900

    請求書発行

--- src/HanbaiKanri/HanbaiKanri/Form/CustomerOrderList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/CustomerOrderList.vb
@@ -7,6 +7,8 @@ Imports UtilMDL.DB
 Imports UtilMDL.DataGridView
 Imports UtilMDL.FileDirectory
 Imports UtilMDL.xls
+Imports Microsoft.Office.Interop
+Imports System.Runtime.InteropServices
 
 
 Public Class CustomerOrderList
@@ -164,72 +166,181 @@ Public Class CustomerOrderList
         Dim Sql2 As String = ""
         Dim Sql3 As String = ""
         Dim reccnt As Integer = 0
+        Dim flg1 As Boolean = False
+        Dim flg2 As Boolean = False
+        Dim flg3 As Boolean = False
+
+        '定義
+        Dim app As Excel.Application = Nothing
+        Dim book As Excel.Workbook = Nothing
+        Dim sheet As Excel.Worksheet = Nothing
 
         For Each r As DataGridViewRow In DgvBilling.SelectedRows
-            BillingNo = DgvBilling.Rows(r.Index).Cells("請求番号").Value
-            OrderNo = DgvBilling.Rows(r.Index).Cells("受注番号").Value
-            OrderSuffix = DgvBilling.Rows(r.Index).Cells("受注番号枝番").Value
-            BillingSubTotal += DgvBilling.Rows(r.Index).Cells("請求金額計").Value
-
-            Sql1 += "SELECT "
-            Sql1 += "* "
-            Sql1 += "FROM "
-            Sql1 += "public"
-            Sql1 += "."
-            Sql1 += "t10_cymnhd"
-            Sql1 += " WHERE "
-            Sql1 += "受注番号"
-            Sql1 += " = "
-            Sql1 += "'"
-            Sql1 += OrderNo
-            Sql1 += "'"
-            Sql1 += " AND "
-            Sql1 += "受注番号枝番"
-            Sql1 += " = "
-            Sql1 += "'"
-            Sql1 += OrderSuffix
-            Sql1 += "'"
-
-            Dim ds1 As DataSet = _db.selectDB(Sql1, RS, reccnt) '受注基本（請求債情報）
-
-            Sql2 += "SELECT "
-            Sql2 += "* "
-            Sql2 += "FROM "
-            Sql2 += "public"
-            Sql2 += "."
-            Sql2 += "t11_cymndt"
-            Sql2 += " WHERE "
-            Sql2 += "受注番号"
-            Sql2 += " = "
-            Sql2 += "'"
-            Sql2 += OrderNo
-            Sql2 += "'"
-            Sql2 += " AND "
-            Sql2 += "受注番号枝番"
-            Sql2 += " = "
-            Sql2 += "'"
-            Sql2 += OrderSuffix
-            Sql2 += "'"
-
-            Dim ds2 As DataSet = _db.selectDB(Sql2, RS, reccnt)　'受注明細（商品情報）
-
-            Sql1 = ""
-            Sql2 = ""
+            If flg1 = False Then
+                BillingNo = DgvBilling.Rows(r.Index).Cells("請求番号").Value
+                flg1 = True
+            End If
         Next r
 
-        Sql3 += "SELECT "
-        Sql3 += "* "
-        Sql3 += "FROM "
-        Sql3 += "public"
-        Sql3 += "."
-        Sql3 += "m01_company"
-        Sql3 += " WHERE "
-        Sql3 += "会社コード"
-        Sql3 += " = "
-        Sql3 += "'"
-        Sql3 += frmC01F10_Login.loginValue.BumonNM
-        Sql3 += "'"
+        Try
+            '雛形パス
+            Dim sHinaPath As String = ""
+            sHinaPath = StartUp._iniVal.BaseXlsPath
 
-        Dim ds3 As DataSet = _db.selectDB(Sql3, RS, reccnt)　'会社情報（振込先情報）
+            '雛形ファイル名
+            Dim sHinaFile As String = ""
+            sHinaFile = sHinaPath & "\" & "Invoice.xlsx"
+
+            '出力先パス
+            Dim sOutPath As String = ""
+            sOutPath = StartUp._iniVal.OutXlsPath
+
+            '出力ファイル名
+            Dim sOutFile As String = ""
+            sOutFile = sOutPath & "\" & BillingNo & ".xlsx"
+
+
+
+            app = New Excel.Application()
+            book = app.Workbooks.Add(sHinaFile)  'テンプレート
+            sheet = CType(book.Worksheets(1), Excel.Worksheet)
+
+
+            BillingNo = ""
+
+            Dim currentRow As Integer = 18
+            Dim lastRow As Integer = 20
+            Dim addRow As Integer = 0
+            Dim currentNum As Integer = 1
+
+
+            For Each r As DataGridViewRow In DgvBilling.SelectedRows
+                BillingNo = DgvBilling.Rows(r.Index).Cells("請求番号").Value
+                OrderNo = DgvBilling.Rows(r.Index).Cells("受注番号").Value
+                OrderSuffix = DgvBilling.Rows(r.Index).Cells("受注番号枝番").Value
+                BillingSubTotal += DgvBilling.Rows(r.Index).Cells("請求金額計").Value
+
+                Sql1 += "SELECT "
+                Sql1 += "* "
+                Sql1 += "FROM "
+                Sql1 += "public"
+                Sql1 += "."
+                Sql1 += "t10_cymnhd"
+                Sql1 += " WHERE "
+                Sql1 += "受注番号"
+                Sql1 += " = "
+                Sql1 += "'"
+                Sql1 += OrderNo
+                Sql1 += "'"
+                Sql1 += " AND "
+                Sql1 += "受注番号枝番"
+                Sql1 += " = "
+                Sql1 += "'"
+                Sql1 += OrderSuffix
+                Sql1 += "'"
+
+                Dim ds1 As DataSet = _db.selectDB(Sql1, RS, reccnt) '受注基本（請求債情報）
+
+                If flg2 = False Then
+                    sheet.Range("B8").Value = ds1.Tables(RS).Rows(0)("得意先名")
+                    sheet.Range("B9").Value = ds1.Tables(RS).Rows(0)("得意先郵便番号")
+                    sheet.Range("B10").Value = ds1.Tables(RS).Rows(0)("得意先住所")
+                    sheet.Range("B14").Value = ds1.Tables(RS).Rows(0)("得意先担当者役職") & " " & ds1.Tables(RS).Rows(0)("得意先担当者名")
+                    sheet.Range("A15").Value = "Telp." & ds1.Tables(RS).Rows(0)("得意先電話番号") & "　Fax." & ds1.Tables(RS).Rows(0)("得意先ＦＡＸ")
+                    sheet.Range("E8").Value = BillingNo
+                    sheet.Range("E9").Value = System.DateTime.Now
+                    sheet.Range("E13").Value = ds1.Tables(RS).Rows(0)("支払条件")
+
+                    flg2 = True
+
+                Else
+                    sheet.Range("E8").Value = sheet.Range("E8").Value & vbLf & BillingNo
+
+                End If
+
+
+                Sql2 += "SELECT "
+                Sql2 += "* "
+                Sql2 += "FROM "
+                Sql2 += "public"
+                Sql2 += "."
+                Sql2 += "t11_cymndt"
+                Sql2 += " WHERE "
+                Sql2 += "受注番号"
+                Sql2 += " = "
+                Sql2 += "'"
+                Sql2 += OrderNo
+                Sql2 += "'"
+                Sql2 += " AND "
+                Sql2 += "受注番号枝番"
+                Sql2 += " = "
+                Sql2 += "'"
+                Sql2 += OrderSuffix
+                Sql2 += "'"
+
+                Dim ds2 As DataSet = _db.selectDB(Sql2, RS, reccnt) '受注明細（商品情報）
+
+                If currentNum > 2 Then
+                    Dim obj As Object
+                    Dim cellPos = lastRow & ":" & lastRow
+                    obj = sheet.Range(cellPos)
+                    obj.Copy()
+                    obj.Insert()
+                    If Marshal.IsComObject(obj) Then
+                        Marshal.ReleaseComObject(obj)
+                    End If
+                    lastRow += 1
+                End If
+
+                sheet.Range("A" & currentRow).Value = currentNum
+                sheet.Range("B" & currentRow).Value = ds2.Tables(RS).Rows(0)("メーカー") & " / " & ds2.Tables(RS).Rows(0)("品名") & " / " & ds2.Tables(RS).Rows(0)("型式")
+                sheet.Range("C" & currentRow).Value = ds2.Tables(RS).Rows(0)("売上数量")
+                sheet.Range("D" & currentRow).Value = ds2.Tables(RS).Rows(0)("売単価")
+                sheet.Range("E" & currentRow).Value = ds2.Tables(RS).Rows(0)("売上金額")
+
+
+
+                currentNum += 1
+                currentRow += 1
+                Sql1 = ""
+                Sql2 = ""
+            Next r
+
+            sheet.Range("E" & lastRow + 1).Value = BillingSubTotal
+            sheet.Range("E" & lastRow + 2).Value = BillingSubTotal * 0.1
+            sheet.Range("E" & lastRow + 3).Value = BillingSubTotal * 1.1
+
+            Sql3 += "SELECT "
+            Sql3 += "* "
+            Sql3 += "FROM "
+            Sql3 += "public"
+            Sql3 += "."
+            Sql3 += "m01_company"
+            Sql3 += " WHERE "
+            Sql3 += "会社コード"
+            Sql3 += " = "
+            Sql3 += "'"
+            Sql3 += frmC01F10_Login.loginValue.BumonNM
+            Sql3 += "'"
+
+            Dim ds3 As DataSet = _db.selectDB(Sql3, RS, reccnt) '会社情報（振込先情報）
+
+
+
+            book.SaveAs(sOutFile)
+            app.Visible = True
+
+            _msgHd.dspMSG("CreateExcel")
+
+        Catch ex As Exception
+            Throw ex
+
+        Finally
+            'app.Quit()
+            'Marshal.ReleaseComObject(sheet)
+            'Marshal.ReleaseComObject(book)
+            'Marshal.ReleaseComObject(app)
+
+        End Try
     End Sub
 End Class
