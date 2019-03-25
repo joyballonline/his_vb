43cbcf0f1aace2d2ded25e829f8eec1379cc4a1c
久慈昌和 <kuji@laevigata.co.jp>
2019-03-25 10:09:20 +0900

    [ 受発注登録 ]英語ユーザでもリードタイム単位が日本語表記となっていたのを修正

--- src/HanbaiKanri/HanbaiKanri/Form/cymn.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/cymn.vb
@@ -107,6 +107,7 @@ Public Class Cymn
         'DataGridView1に追加する
         DgvItemList.Columns.Insert(1, column)
 
+        'リードタイム単位の設定
         Dim reccnt As Integer = 0
         Dim Sql12 As String = ""
 
@@ -121,7 +122,12 @@ Public Class Cymn
         table2.Columns.Add("Value", GetType(Integer))
 
         For index As Integer = 0 To ds12.Tables(RS).Rows.Count - 1
-            table2.Rows.Add(ds12.Tables(RS).Rows(index)("文字１"), ds12.Tables(RS).Rows(index)("可変キー"))
+            If frmC01F10_Login.loginValue.Language = CommonConst.LANG_KBN_ENG Then
+                table2.Rows.Add(ds12.Tables(RS).Rows(index)("文字２"), ds12.Tables(RS).Rows(index)("可変キー"))
+            Else
+                table2.Rows.Add(ds12.Tables(RS).Rows(index)("文字１"), ds12.Tables(RS).Rows(index)("可変キー"))
+            End If
+
         Next
 
         Dim column2 As New DataGridViewComboBoxColumn()
