8e14c4abf2d474e52a9f3a5f545ab6bec8476ef1
eri kasahara <kasahara@laevigata.co.jp>
2019-04-11 11:35:20 +0900

    [ #513 ] 仕入先別買掛金一覧：Excel出力時のキャンセルエラー対応

--- src/HanbaiKanri/HanbaiKanri/Form/SupplierAPList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/SupplierAPList.vb
@@ -10,6 +10,7 @@ Imports UtilMDL.xls
 Imports System.Globalization
 Imports System.Text.RegularExpressions
 Imports Microsoft.Office.Interop
+Imports System.IO
 
 Public Class SupplierAPList
     Inherits System.Windows.Forms.Form
@@ -220,7 +221,15 @@ Public Class SupplierAPList
 
             Next
 
-            book.SaveAs(sOutFile)
+            app.DisplayAlerts = False 'Microsoft Excelのアラート一旦無効化
+
+            Dim excelChk As Boolean = excelOutput(sOutFile)
+            If excelChk = False Then
+                Exit Sub
+            End If
+            book.SaveAs(sOutFile) '書き込み実行
+
+            app.DisplayAlerts = True 'アラート無効化を解除
 
             'カーソルをビジー状態から元に戻す
             Cursor.Current = Cursors.Default
@@ -284,6 +293,31 @@ Public Class SupplierAPList
 
         '日本の形式に書き換える
         Return changeFormat
+    End Function
+
+    'Excel出力する際のチェック
+    Private Function excelOutput(ByVal prmFilePath As String)
+        Dim fileChk As String = Dir(prmFilePath)
+        '同名ファイルがあるかどうかチェック
+        If fileChk <> "" Then
+            Dim result = _msgHd.dspMSG("confirmFileExist", frmC01F10_Login.loginValue.Language, prmFilePath)
+            If result = DialogResult.No Then
+                Return False
+            End If
+
+            Try
+                'ファイルが開けるかどうかチェック
+                Dim sr As StreamReader = New StreamReader(prmFilePath)
+                sr.Close() '処理が通ったら閉じる
+            Catch ex As Exception
+                '開けない場合はアラートを表示してリターンさせる
+                MessageBox.Show(ex.Message, CommonConst.AP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return False
+            End Try
+
+            Return True
+        End If
+        Return True
     End Function
 
 End Class
