a6f59a5d87b5d3d23b7d0c500b7772bce1046571
熊谷 亜祐美 <kumagai@laevigata.co.jp>
2019-04-23 15:11:41 +0900

    売上参照　SalesList　売上番号検索 見た目の変更と範囲指定から部分一致へ変更

--- src/HanbaiKanri/HanbaiKanri/Form/SalesList.Designer.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/SalesList.Designer.vb
@@ -26,14 +26,12 @@ Partial Class SalesList
         Me.BtnSalesView = New System.Windows.Forms.Button()
         Me.Label10 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
-        Me.TxtSalesUntil = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.TxtSales = New System.Windows.Forms.TextBox()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.TxtSalesSince = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
         Me.TxtCustomerCode = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.TxtTel = New System.Windows.Forms.TextBox()
@@ -96,14 +94,6 @@ Partial Class SalesList
         Me.Label5.Size = New System.Drawing.Size(17, 12)
         Me.Label5.TabIndex = 87
         Me.Label5.Text = "～"
-        '
-        'TxtSalesUntil
-        '
-        Me.TxtSalesUntil.Font = New System.Drawing.Font("MS UI Gothic", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
-        Me.TxtSalesUntil.Location = New System.Drawing.Point(959, 68)
-        Me.TxtSalesUntil.Name = "TxtSalesUntil"
-        Me.TxtSalesUntil.Size = New System.Drawing.Size(170, 22)
-        Me.TxtSalesUntil.TabIndex = 8
         '
         'Label6
         '
@@ -168,15 +158,6 @@ Partial Class SalesList
         Me.Label4.TabIndex = 78
         Me.Label4.Text = "得意先コード"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label9
-        '
-        Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(936, 74)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(17, 12)
-        Me.Label9.TabIndex = 88
-        Me.Label9.Text = "～"
         '
         'TxtCustomerCode
         '
@@ -397,14 +378,12 @@ Partial Class SalesList
         Me.Controls.Add(Me.BtnSalesView)
         Me.Controls.Add(Me.Label10)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.TxtSalesUntil)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.TxtSales)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.TxtSalesSince)
         Me.Controls.Add(Me.Label8)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.TxtCustomerCode)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.TxtTel)
@@ -430,14 +409,12 @@ Partial Class SalesList
     Friend WithEvents BtnSalesView As Button
     Friend WithEvents Label10 As Label
     Friend WithEvents Label5 As Label
-    Friend WithEvents TxtSalesUntil As TextBox
     Friend WithEvents Label6 As Label
     Friend WithEvents TxtSales As TextBox
     Friend WithEvents Label7 As Label
     Friend WithEvents TxtSalesSince As TextBox
     Friend WithEvents Label8 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label9 As Label
     Friend WithEvents TxtCustomerCode As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents TxtTel As TextBox
