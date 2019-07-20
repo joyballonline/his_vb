ff21a5cbfd282358012a5c2e7a94f5984c292bdf
tateiwa1900 <30627204+tateiwa1900@users.noreply.github.com>
2019-07-20 11:50:24 +0900

    [入金登録画面] レートの取得条件を基準日未満に変更

--- src/HanbaiKanri/HanbaiKanri/Form/DepositManagement.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/DepositManagement.vb
@@ -881,12 +881,12 @@ Public Class DepositManagement
     End Sub
 
     '通貨の採番キーからレートを取得・設定
-    '基準日が請求日以前の最新のもの
+    '基準日が入金日「以前」の最新のもの
     Private Function setRate(ByVal strKey As Integer) As Decimal
         Dim Sql As String
 
         Sql = " AND 採番キー = " & strKey & ""
-        Sql += " AND 基準日 <= '" & UtilClass.strFormatDate(DtpDepositDate.Text) & "'"  '入金日
+        Sql += " AND 基準日 < '" & UtilClass.strFormatDate(DtpDepositDate.Text) & "'"  '入金日
         Sql += " ORDER BY 基準日 DESC "
 
         Dim ds As DataSet = getDsData("t71_exchangerate", Sql)
