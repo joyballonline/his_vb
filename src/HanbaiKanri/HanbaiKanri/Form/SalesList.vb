2532983c9689f9516d07a40465b502fe75a0c3f4
tateiwa1900 <30627204+tateiwa1900@users.noreply.github.com>
2019-07-28 17:42:04 +0900

    [売上取消画面] 明細時に取消ボタンを押下しても動作しないよう修正

--- src/HanbaiKanri/HanbaiKanri/Form/SalesList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/SalesList.vb
@@ -546,6 +546,14 @@ Public Class SalesList
             Return
         End If
 
+        '明細表示時は取消操作不可能
+        If RbtnDetails.Checked Then
+
+            '操作できないアラートを出す
+            _msgHd.dspMSG("NonAction", frmC01F10_Login.loginValue.Language)
+            Return
+        End If
+
         '取消済みデータは取消操作不可能
         If DgvCymnhd.Rows(DgvCymnhd.CurrentCell.RowIndex).Cells("取消").Value = CommonConst.CANCEL_KBN_DISABLED_TXT Then
             '取消データは選択できないアラートを出す
