20d92561fcac33763701f471ea413ae1b6d8dfb1
tateiwa <tateiwa@ict-tt.com>
2019-11-10 11:04:23 +0900

    [出庫取消] 明細単位表示で取消ボタンを押下した場合のメッセージを変更

--- src/HanbaiKanri/HanbaiKanri/Form/GoodsIssueList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/GoodsIssueList.vb
@@ -478,7 +478,7 @@ Public Class GoodsIssueList
         If RbtnDetails.Checked Or DgvCymnhd.Rows.Count = 0 Then
 
             '操作できないアラートを出す
-            _msgHd.dspMSG("NonAction", frmC01F10_Login.loginValue.Language)
+            _msgHd.dspMSG("chkDetailsCancel", frmC01F10_Login.loginValue.Language)
             Return
 
         End If
