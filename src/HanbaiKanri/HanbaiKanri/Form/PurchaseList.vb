0811792dc6ae769f16b1f3cad3460e7083d540c0
tateiwa <tateiwa@ict-tt.com>
2019-11-10 10:57:28 +0900

    [仕入取消] 明細単位表示で取消ボタンを押下した場合のメッセージを変更

--- src/HanbaiKanri/HanbaiKanri/Form/PurchaseList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/PurchaseList.vb
@@ -628,7 +628,7 @@ Public Class PurchaseList
         If RbtnDetails.Checked Or DgvHtyhd.Rows.Count = 0 Then
 
             '操作できないアラートを出す
-            _msgHd.dspMSG("NonAction", frmC01F10_Login.loginValue.Language)
+            _msgHd.dspMSG("chkDetailsCancel", frmC01F10_Login.loginValue.Language)
             Return
 
         End If
