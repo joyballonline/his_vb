01dd4475356273752555c139ac85018406b62029
tateiwa <tateiwa@ict-tt.com>
2019-11-10 10:33:31 +0900

    [売上取消] 明細単位表示で取消ボタンを押下した場合のメッセージを変更

--- src/HanbaiKanri/HanbaiKanri/Form/SalesList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/SalesList.vb
@@ -621,7 +621,7 @@ Public Class SalesList
         If RbtnDetails.Checked Then
 
             '操作できないアラートを出す
-            _msgHd.dspMSG("NonAction", frmC01F10_Login.loginValue.Language)
+            _msgHd.dspMSG("chkDetailsCancel", frmC01F10_Login.loginValue.Language)
             Return
         End If
 
