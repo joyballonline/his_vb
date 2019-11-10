026521fb421768c27f0bee77d4c5afa298f7ff67
tateiwa <tateiwa@ict-tt.com>
2019-11-10 10:36:45 +0900

    [入金取消] 明細単位表示で取消ボタンを押下した場合のメッセージを変更

--- src/HanbaiKanri/HanbaiKanri/Form/DepositDetailList.vb
+++ src/HanbaiKanri/HanbaiKanri/Form/DepositDetailList.vb
@@ -395,7 +395,7 @@ Public Class DepositDetailList
         If RbtnDetails.Checked Or DgvBilling.Rows.Count = 0 Then
 
             '操作できないアラートを出す
-            _msgHd.dspMSG("NonAction", frmC01F10_Login.loginValue.Language)
+            _msgHd.dspMSG("chkDetailsCancel", frmC01F10_Login.loginValue.Language)
             Return
 
         End If
