f95f01246cd94f0517c0cce47497fbc298b10bb0
fuketa <fuketecs.software@gmail.com>
2021-03-02 22:48:24 +0700

    Auto delivery function

---
+++ src/HanbaiKanri/HanbaiKanri/bin/SQL/t82_tmp_order.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE public.t82_tmp_order(
+会社コード varchar(5) default 'ZENBI',
+	order_no varchar(50) NULL,
+	x varchar(50) NULL
+);
+
+ALTER TABLE public.t82_tmp_order ADD 会社コード varchar(5) NULL;
